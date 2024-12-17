1156185a20550a5f3de80cdfe57c5db58f935e2e
StrayBeing <maciej.sobierajski.zsti@gmail.com>
2024-12-17 12:46:34 +0100

    Lab 12

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import styles from './page.module.css';
+import Link from 'next/link';
 import ImageSlideshow from './/components/images/image-slideshow';
 export default function Home() {
   return (
@@ -9,8 +10,8 @@ export default function Home() {
           <h1>NextLevel Food for NextLevel Foodies!</h1>
           <p>Taste & share food from all over the world.</p>
           <div className={styles.cta}>
-            <a href="/community">Join the Community</a>
-            <a href="/meals">Explore Meals</a>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
           </div>
         </div>
       </header>
